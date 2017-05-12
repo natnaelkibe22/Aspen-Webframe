0826e412e53e132b44a4d6dbb7fcf28c7295a7fc
Conlan <conlanc@gmail.com>
2017-05-12 15:14:21 -0400

    Version 2
    Use Bootstrap, add Lunch, add Day Progress, update for AspenAPI JSON v2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var today = new Date();
+
 // Async HTTP GET Function
 var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709894!
   this.get = function(aUrl, aCallback) {
@@ -12,22 +14,36 @@ var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709
   }
 }
 
-function main(){
-loggedOutAspen = new HttpClient();
-loggedOutAspen.get('https://mhs-aspencheck-serve.herokuapp.com', function(response) {
-  var aspenInfo = JSON.parse(response);
-  var block = (aspenInfo.block);
-  var day = (aspenInfo.day);
+function getLunchInfo(lunchMenu) { // Bypassing CORS using JSONP
+  var lunchSpecial = (lunchMenu.days[today.getDay() - 1].menu_items[1].food.name);
 
-  document.getElementById('dayNumber').innerHTML = day;
-  if (block != 'Z'){
-  	document.getElementById('blockId').innerHTML = block;
-  } else {
-	document.getElementsById('block').className = " fadeHidden";
+  document.getElementById('lunch-body').innerHTML = lunchSpecial;
 }
 
-  // Once loading is complete, render page
-  document.getElementById('load').className += " fadeHidden";
-  alert("ehllo");
-});
+function main(){
+  // Aspen Stuff
+  loggedOutAspen = new HttpClient();
+  try {
+    loggedOutAspen.get('https://mhs-aspencheck-serve.herokuapp.com', function(response) {
+      var aspenInfo = JSON.parse(response);
+      var block = (aspenInfo.schedule.block);
+      var day = (aspenInfo.schedule.day);
+      var blockOfDay = (aspenInfo.schedule.blockOfDay);
+
+      document.getElementById('dayNumber').innerHTML = day;
+      document.getElementById('blockId').innerHTML = block;
+      document.getElementById('dayProgress').style = "width: " + (blockOfDay/6)*100 +"%;";
+      if (block == 'Z') document.getElementById('block-panel').className += " fadeHidden";
+
+      // Once loading is complete, render page
+      document.getElementById('aspenLoadingSpinner').className += " fadeHidden";
+    });
+  } catch (error) {
+    document.getElementById('fetchIssue').style += 'display:inherit;'
+  }
+
+  // Eww JSONP (Thanks, CORS!)
+  var lunchtag = document.createElement("script");
+  lunchtag.src = "https://melroseschools.nutrislice.com/menu/api/weeks/school/melrose/menu-type/lunch/" + today.getFullYear() + "/00/00/?format=json-p&callback=getLunchInfo";
+  document.getElementsByTagName('head')[0].appendChild(lunchtag);
 }
