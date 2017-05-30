1924c55970e5e0812878f6ddc185b68a668f00b3
Conlan <conlanc@gmail.com>
2017-05-30 13:42:38 -0400

    Happy now, Codacy?

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709
   this.get = function(aUrl, aCallback) {
     var anHttpRequest = new XMLHttpRequest();
     anHttpRequest.onreadystatechange = function() {
-      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+      if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
         aCallback(anHttpRequest.responseText);
-    }
+    };
 
     anHttpRequest.open( "GET", aUrl, true );
     anHttpRequest.send( null );
@@ -24,6 +24,37 @@ function getLunchInfo(lunchMenu) { // Bypassing CORS using JSONP
   }
 }
 
+function clock(isHalfDay){
+  // Thanks to http://stackoverflow.com/a/36524883/1709894 and https://www.w3schools.com/howto/howto_js_countdown.asp
+  var countDownDate = new Date();
+  var time =+ countDownDate;
+
+  if (isHalfDay){
+    countDownDate.setHours(12,11,10,0);
+  } else {
+    countDownDate.setHours(14,11,10,0);
+  }
+  if (countDownDate < time) {
+    countDownDate.setDate(countDownDate.getDate() + 1);
+  }
+
+// Update the count down every 1 second
+var timer = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}
+
 function main(){
   // Eww JSONP (Thanks, CORS!)
   var lunchtag = document.createElement("script");
@@ -31,7 +62,7 @@ function main(){
   document.getElementsByTagName('head')[0].appendChild(lunchtag);
 
   // Aspen Stuff
-  loggedOutAspen = new HttpClient();
+  var loggedOutAspen = new HttpClient();
   try {
     loggedOutAspen.get('https://mhs-aspencheck-serve.herokuapp.com', function(response) {
       var aspenInfo = JSON.parse(response);
@@ -53,8 +84,8 @@ function main(){
       document.getElementById('blockId').innerHTML = block;
       document.getElementById('dayProgress').setAttribute('style', 'width: ' + percentComplete + '%;');
       document.getElementById('dayProgress').innerHTML = Math.round(percentComplete) + '%';
-      if (classInSession) document.getElementById('dayProgress').setAttribute('class', 'progress-bar progress-bar-striped progress-bar-danger active');
-      if (block == 'Z') document.getElementById('block-panel').className += " fadeHidden";
+      if (classInSession) { document.getElementById('dayProgress').setAttribute('class', 'progress-bar progress-bar-striped progress-bar-danger active'); }
+      if (block === 'Z') { document.getElementById('block-panel').className += " fadeHidden"; }
       document.getElementById('lastUpdated').innerHTML = (lastUpdated.getMonth() + 1) + "/" + (lastUpdated.getDate()) + " " + (lastUpdated.getHours()) + ":" + (lastUpdated.getMinutes()) + ":" + (lastUpdated.getSeconds());
 
       if (events.length > 0){
@@ -79,36 +110,4 @@ function main(){
     document.getElementById('fetchIssue').setAttribute('style', 'display:inherit;');
     clock(false);
   }
-}
-
-function clock(isHalfDay){
-  // Thanks to http://stackoverflow.com/a/36524883/1709894 and https://www.w3schools.com/howto/howto_js_countdown.asp
-  var countDownDate = new Date();
-  var time = +countDownDate;
-
-  if (isHalfDay){
-    countDownDate.setHours(12,11,10,0);
-  } else {
-    countDownDate.setHours(14,11,10,0);
-  }
-  if (countDownDate < time) {
-    countDownDate.setDate(countDownDate.getDate() + 1);
-  }
-
-// Update the count down every 1 second
-var timer = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
 }
